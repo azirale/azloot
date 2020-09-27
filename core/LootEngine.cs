4641441dfc8c35d29fb49be7e575b924dc78905a
Azirale <azirale@gmail.com>
2020-09-27 15:28:24 +1000

    Expanding loot engine capability

--- core/LootEngine.cs
+++ core/LootEngine.cs
@@ -7,22 +7,52 @@ namespace azloot.core
     /// This is the custom part. The loot engine should work with the data structure available to maintain points,
     /// and to distribute loot from a pool.
     /// </summary>
-    public class LootEngine
+    public abstract class LootEngine
     {
-        private Configuration config { get; set; }
+        private readonly Configuration config;
+        private LootPool activePool;
 
-        public List<Bid> GetSortedBids(List<Bid> existingBids)
+        public LootEngine()
         {
-            var sortedBids = new List<Bid>(existingBids);
-            sortedBids.Sort(BidComparator);
-            return sortedBids;
+            this.config = new Configuration();
+        }
+
+        public LootEngine(Configuration config)
+        {
+            this.config = config;
+        }
+
+        public LootEngine(ConfigurationDatapack datapack)
+        {
+            this.config = new Configuration(datapack);
+        }
+
+
+        public void AddPoints(PointsList pointsList, List<Person> persons, float addValue, string reason)
+        {
+            foreach (Person person in persons)
+            {
+                pointsList[person] += addValue;
+            }
+            var newPointsEvent = new PointsEvent()
+            {
+                Id = Guid.NewGuid(),
+                PointsList = pointsList,
+                Recipients = persons,
+                Timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+                Value = addValue,
+                Reason = reason
+            };
+            this.config.PointsHistory.Add(newPointsEvent);
         }
 
         public void AssignLootFromPool(LootPool pool)
         {
-            var sortedBids = GetSortedBids(pool.Bids);
-            if (sortedBids.Count == 0) return; // nothing to do
-            var winningBid = sortedBids[0];
+            // nothing to do if there are no bids
+            if (pool.Bids.Count == 0) return;
+            // sort the bids in-place to see which is highest priority
+            SortBids(pool.Bids);
+            var winningBid = pool.Bids[0];
             // assign loot based on the winning bid
             AssignLootDirectly(winningBid.Item, winningBid.Person, winningBid.PrioList);
             // this item should no longer be in the pool
@@ -43,7 +73,7 @@ namespace azloot.core
         {
             var newLootEvent = new LootEvent()
             {
-                Id = new Guid(),
+                Id = Guid.NewGuid(),
                 Item = item,
                 Person = person,
                 PointsList = pointsList,
@@ -53,33 +83,35 @@ namespace azloot.core
             var currentPoints = pointsList[person];
             var newPointsEvent = new PointsEvent()
             {
-                Id = new Guid(),
+                Id = Guid.NewGuid(),
                 PointsList = pointsList,
                 Recipients = new List<Person>() { person },
                 Timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
                 Value = -currentPoints,
-                Reason = String.Format("Won item {0} ({1}) on list {2}", item.Id, item.Name, pointsList.Name)
+                Reason = string.Format("Won item {0} ({1}) on list {2}", item.Id, item.Name, pointsList.Name)
             };
             this.config.PointsHistory.Add(newPointsEvent);
             pointsList[person] = 0;
         }
 
-        public void AddPoints(PointsList pointsList, List<Person> persons, float addValue, string reason)
+        public void SortBids(List<Bid> allBids)
         {
-            foreach (Person person in persons)
+            // first we need to align personal priorities to be simply ordered regardless of original numbers used
+            // group by person
+            var personBids = new Dictionary<Guid, List<Bid>>();
+            foreach (var bid in allBids)
             {
-                pointsList[person] += addValue;
+                if (!personBids.ContainsKey(bid.Person.Id)) personBids[bid.Person.Id] = new List<Bid>();
+                personBids[bid.Person.Id].Add(bid);
             }
-            var newPointsEvent = new PointsEvent()
+            // for each person sort their bids by their personal priority, then alter the priority to be 0-indexed
+            foreach (var bidList in personBids.Values)
             {
-                Id = new Guid(),
-                PointsList = pointsList,
-                Recipients = persons,
-                Timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
-                Value = addValue,
-                Reason = reason
-            };
-            this.config.PointsHistory.Add(newPointsEvent);
+                bidList.Sort((a, b) => { return a.PersonalPriority.CompareTo(b.PersonalPriority); });
+                for (int i = 0; i < bidList.Count; i++) bidList[i].PersonalPriority = i;
+            }
+            // now that personal priority values are aligned we can sort the data
+            allBids.Sort(BidComparator);
         }
 
         /// <summary>
@@ -106,24 +138,16 @@ namespace azloot.core
             var otherPersonLastLoot = this.config.LootHistory.GetLastLootForPerson(one.Person)?.Timestamp ?? long.MinValue;
             var personLastLootOrder = thisPersonLastLoot.CompareTo(otherPersonLastLoot);
             if (personLastLootOrder != 0) return personLastLootOrder;
-            // if all that is equal, if on person has prioritised an item higher than the other they can get it first
+            // if all that is equal, if one person has prioritised an item higher than the other they can get it first
             var personalPriorityOrder = one.PersonalPriority.CompareTo(two.PersonalPriority);
             if (personalPriorityOrder != 0) return personalPriorityOrder;
             // still the same? consistent 'coin flip' it on guid
             var personCoinflip = one.Person.Id.CompareTo(two.Person.Id);
             if (personCoinflip != 0) return personCoinflip;
-            // if the same player has submitted multiple builds on equal lists at equal prio coinflip the items
+            // if the same player has submitted multiple bids on equal lists at equal prio coinflip the items
             return one.Item.Id.CompareTo(two.Item.Id);
         }
 
-        public void CreatePointslist(string Name)
-        {
-
-        }
-
-        /* 
-         * 
-         */
 
     }
 }
